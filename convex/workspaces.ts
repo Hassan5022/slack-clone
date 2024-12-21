003020a7d1caee176e52469073edd1fce878deb6
hassan5022 <hassanahmed5022@gmail.com>
2024-12-21 18:23:56 +0500

    indexing in wokspaces

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,8 +1,18 @@
-import { mutationGeneric, queryGeneric } from "convex/server";
 import { v } from "convex/values";
+
+import { mutationGeneric, queryGeneric } from "convex/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 // api endpoint
 // ctx = context
+
+const generateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+
+  return code;
+};
 
 export const create = mutationGeneric({
   args: {
@@ -13,8 +23,7 @@ export const create = mutationGeneric({
 
     if (!userId) throw new Error("Unauthorized");
 
-    // Todo: Create a proper method later
-    const joinCode = "123456";
+    const joinCode = generateCode();
 
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
@@ -35,7 +44,27 @@ export const create = mutationGeneric({
 export const get = queryGeneric({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await getAuthUserId(ctx);
+    if (!userId) return [];
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) workspaces.push(workspace);
+    }
+
+    console.log("workspaces :", workspaces);
+
+    return workspaces;
   },
 });
 
@@ -47,6 +76,14 @@ export const getById = queryGeneric({
     if (!userId) {
       throw new Error("Unautherized");
     }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) => q.eq("workspaceId", args.id))
+      .filter((q) => q.eq(q.field("userId"), userId))
+      .unique();
+
+    if (!member) return null;
 
     return await ctx.db.get(args.id);
   },
